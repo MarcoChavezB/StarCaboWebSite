62674db36f5b13db158fbc661b1d3d212ba89b9b
Marco <marco1102004@gmail.com>
2024-01-06 11:00:28 -0600

    7

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/StarCaboWebSite/', // Ajusta según sea necesario
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, 'src'),
+    },
+  },
+  base: '/',
   build: {
     assetsDir: 'assets',
   },
